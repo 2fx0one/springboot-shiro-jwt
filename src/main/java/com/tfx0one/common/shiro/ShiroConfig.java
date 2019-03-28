af34c4173f19cc2490912fd779749b675e7987c4
2fx0one <2fx0one@gmail.com>
2019-03-28 17:47:37 +0800

    update

--- src/main/java/com/tfx0one/common/shiro/ShiroConfig.java
+++ src/main/java/com/tfx0one/common/shiro/ShiroConfig.java
@@ -5,13 +5,17 @@ import org.apache.shiro.mgt.DefaultSubjectDAO;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
+import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.crazycake.shiro.RedisCacheManager;
 import org.crazycake.shiro.RedisManager;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Primary;
 
 import javax.servlet.Filter;
 import java.util.HashMap;
@@ -38,26 +42,44 @@ public class ShiroConfig {
      *
      * @return
      */
-//    @Bean
+
+    @Bean
+    public ShiroFilterChainDefinition shiroFilterChainDefinition() {
+        DefaultShiroFilterChainDefinition chainDefinition = new DefaultShiroFilterChainDefinition();
+
+//        // logged in users with the 'admin' role
+//        chainDefinition.addPathDefinition("/admin/**", "authc, roles[admin]");
+//
+//        // logged in users with the 'document:read' permission
+//        chainDefinition.addPathDefinition("/docs/**", "authc, perms[document:read]");
+
+        // all other paths require a logged in user
+        chainDefinition.addPathDefinition("/**", "anon");
+        return chainDefinition;
+    }
+
+    //    @Bean
 //    public EhCacheManager ehCacheManager() {
 //        EhCacheManager cacheManager = new EhCacheManager();
 //        cacheManager.setCacheManagerConfigFile("classpath:ehcache.xml");
 //        return cacheManager;
 //    }
     @Bean("redisCacheManager")
+    @Primary
     public RedisCacheManager redisCacheManager() {
         RedisCacheManager redisCacheManager = new RedisCacheManager();
         redisCacheManager.setRedisManager(new RedisManager());
+        redisCacheManager.setExpire(3600); //一小时
         return redisCacheManager;
     }
 
 
     @Bean("securityManager")
-    public DefaultWebSecurityManager manger(ShiroAuthRealm shiroAuthRealm, RedisCacheManager redisCacheManager) {
+    public DefaultWebSecurityManager manger(ShiroAuthRealm shiroAuthRealm, RedisCacheManager cacheManager) {
         DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
 
         //缓存
-        manager.setCacheManager(redisCacheManager);
+        manager.setCacheManager(cacheManager);
 
         //自己的 customShiroRealm
         shiroAuthRealm.setAuthenticationCacheName(AUTHENTICATION_CACHE_NAME);
