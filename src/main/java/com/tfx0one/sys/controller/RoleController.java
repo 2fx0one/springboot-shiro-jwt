54b7d751d1e5e0cb82ff0290a9f0f8bd0b004884
2fx0one <2fx0one@gmail.com>
2019-04-30 17:25:11 +0800

    update

--- src/main/java/com/tfx0one/sys/controller/RoleController.java
+++ src/main/java/com/tfx0one/sys/controller/RoleController.java
@@ -1,9 +1,17 @@
 package com.tfx0one.sys.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.tfx0one.common.api.R;
+import com.tfx0one.sys.entity.Menu;
+import com.tfx0one.sys.entity.Role;
+import com.tfx0one.sys.service.MenuService;
+import com.tfx0one.sys.service.RoleService;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
 import com.tfx0one.common.base.BaseController;
 
 /**
@@ -15,7 +23,33 @@ import com.tfx0one.common.base.BaseController;
  * @since 2019-03-28
  */
 @RestController
-@RequestMapping("/sys/role")
+@RequestMapping("/api/sys/role")
+@RequiresAuthentication
+@RequiresRoles({"admin"})
 public class RoleController extends BaseController {
+    @Autowired
+    private RoleService roleService;
 
+    @GetMapping("view")
+    public ResponseEntity sysMenuView() {
+        return R.ok(roleService.list());
+    }
+
+    @PostMapping("add")
+    public ResponseEntity sysMenuAdd(@RequestBody Role role) {
+        roleService.save(role);
+        return R.ok("添加成功！");
+    }
+
+    @PutMapping("modify")
+    public ResponseEntity sysMenuModify(@RequestBody Role role) {
+        roleService.updateById(role);
+        return R.ok("修改成功！");
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity sysMenuDelete(@PathVariable String id) {
+        roleService.removeById(id);
+        return R.ok("删除成功！");
+    }
 }
