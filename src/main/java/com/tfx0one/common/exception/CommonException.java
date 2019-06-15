11c7b7c51b888f7ed04b0898bea0b964b26a98d3
2fx0one <2fx0one@gmail.com>
2019-06-15 10:24:01 +0800

    update

--- src/main/java/com/tfx0one/common/exception/CommonException.java
+++ src/main/java/com/tfx0one/common/exception/CommonException.java
@@ -1,15 +1,13 @@
 package com.tfx0one.common.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 /*
  * @Auth 2fx0one
  * 2019/3/13 01:04
  */
 @Getter
-public class CommonException extends RuntimeException{
+public class CommonException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
     private String msg;
