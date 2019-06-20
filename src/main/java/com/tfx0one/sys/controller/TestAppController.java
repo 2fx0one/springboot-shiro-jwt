c64d1779d8c2f8920de377510b4735d55da74fac
2fx0one <2fx0one@gmail.com>
2019-06-20 18:11:08 +0800

    update

--- src/main/java/com/tfx0one/sys/controller/TestAppController.java
+++ src/main/java/com/tfx0one/sys/controller/TestAppController.java
@@ -2,12 +2,6 @@ package com.tfx0one.sys.controller;
 
 import com.tfx0one.common.api.R;
 import com.tfx0one.common.base.BaseController;
-import com.tfx0one.common.utils.ShiroUtils;
-import com.tfx0one.sys.entity.User;
-import com.tfx0one.sys.service.MenuService;
-import org.apache.shiro.authz.annotation.RequiresAuthentication;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/app/")
 public class TestAppController extends BaseController {
-
 
 
     @GetMapping("/a")
