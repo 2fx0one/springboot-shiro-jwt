fcfad5899aef24b1b62dffb0c0f17ad5c06067c3
2fx0one <2fx0one@gmail.com>
2019-07-17 23:26:39 +0800

    update

---
+++ src/main/java/com/tfx0one/modules/ec/controller/CategoryController.java
@@ -0,0 +1,90 @@
+package com.tfx0one.modules.ec.controller;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tfx0one.modules.ec.entity.CategoryEntity;
+import com.tfx0one.modules.ec.service.CategoryService;
+import com.tfx0one.common.utils.Pagination;;
+import com.tfx0one.common.utils.R;
+
+
+
+/**
+ * 商品类目表，类目和商品(spu)是一对多关系，类目与品牌是多对多关系
+ *
+ * @author 2fx0one
+ * @email 2fx0one@gmail.com
+ * @date 2019-07-17 23:04:34
+ */
+@RestController
+@RequestMapping("/ec/category")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 列表
+     */
+    @RequestMapping("/list")
+    @RequiresPermissions("ec:category:list")
+    public R list(@RequestParam Map<String, Object> params){
+        Pagination page = categoryService.queryPage(params);
+
+        return R.ok(page);
+    }
+
+
+    /**
+     * 信息
+     */
+    @RequestMapping("/info/{id}")
+    @RequiresPermissions("ec:category:info")
+    public R info(@PathVariable("id") Long id){
+		CategoryEntity category = categoryService.getById(id);
+
+        return R.ok(category);
+    }
+
+    /**
+     * 保存
+     */
+    @RequestMapping("/save")
+    @RequiresPermissions("ec:category:save")
+    public R save(@RequestBody CategoryEntity category){
+		categoryService.save(category);
+
+        return R.ok("保存成功！");
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update")
+    @RequiresPermissions("ec:category:update")
+    public R update(@RequestBody CategoryEntity category){
+		categoryService.updateById(category);
+
+        return R.ok("修改成功！");
+    }
+
+    /**
+     * 删除
+     */
+    @RequestMapping("/delete")
+    @RequiresPermissions("ec:category:delete")
+    public R delete(@RequestBody Long[] ids){
+		categoryService.removeByIds(Arrays.asList(ids));
+
+        return R.ok("删除成功！");
+    }
+
+}
---
+++ src/main/java/com/tfx0one/modules/ec/entity/CategoryEntity.java
@@ -0,0 +1,45 @@
+package com.tfx0one.modules.ec.entity;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+import lombok.experimental.Accessors;
+/**
+ * 商品类目表，类目和商品(spu)是一对多关系，类目与品牌是多对多关系
+ * 
+ * @author 2fx0one
+ * @email 2fx0one@gmail.com
+ * @date 2019-07-17 23:04:34
+ */
+@Data
+@TableName("ec_category")
+@Accessors(chain = true)
+public class CategoryEntity implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 类目id
+	 */
+	@TableId
+	private Long id;
+	/**
+	 * 类目名称
+	 */
+	private String name;
+	/**
+	 * 父类目id,顶级类目填0
+	 */
+	private Long parentId;
+	/**
+	 * 是否为父节点，0为否，1为是
+	 */
+	private Integer isParent;
+	/**
+	 * 排序指数，越小越靠前
+	 */
+	private Integer sort;
+
+}
