6847c93aef9cfd615c86f91e800b87b8b98459ce
2fx0one <2fx0one@gmail.com>
2019-09-26 15:22:57 +0800

    update

--- src/main/java/com/tfx0one/modules/app/utils/AppJWTUtils.java
+++ src/main/java/com/tfx0one/modules/app/utils/AppJWTUtils.java
@@ -18,6 +18,7 @@ import java.util.Date;
 public class AppJWTUtils {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
+    private String a_b;
     private String secret;
     private long expire;
     private String header;
