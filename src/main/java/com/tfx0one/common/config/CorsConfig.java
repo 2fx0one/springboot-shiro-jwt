bc22b126d8e2bd473a0db9ac2df77f9cc34206f0
2fx0one <2fx0one@gmail.com>
2019-11-11 17:09:11 +0800

    update

--- src/main/java/com/tfx0one/common/config/CorsConfig.java
+++ src/main/java/com/tfx0one/common/config/CorsConfig.java
@@ -5,9 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 //@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    /**
+    * @Author wangk
+    * @Description 跨域由NGINX 解决
+    * @Date 17:07 2019-11-11
+    **/
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
